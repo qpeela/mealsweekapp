174f39aa4ae35c17b1186d96bf3223110e43ed6e
Vladmir <qpeela@gmail.com>
2022-07-06 09:41:42 +0500

    change  api method to class

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,54 +1,86 @@
+import { notification } from 'antd';
+
 import { FormAuthValues } from '../type/common';
 import { TOKEN_NAME } from './AuthWrapper';
 
 type AuthLoginRes = { access_token?: string };
 
+const FETCH_OPTIONS = {
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+    }
+};
+
 function checkLoginResponse(data: AuthLoginRes) {
-  if (data?.access_token) {
-    return true;
-  }
-  throw new Error('Ошибка при получение токена');
+    if (data?.access_token) {
+        return true;
+    }
+    throw new Error('Ошибка при получение токена');
 }
 
 function checkResponse(response: Response) {
-  if (!response.ok) {
-    throw response;
-  }
+    if (!response.ok) {
+        throw response;
+    }
 }
 
-export class Api {
-  handleError(error: Response) {
+function handleError(error: Response) {
     if (error.status === 401) {
-      console.log('Ошибка авторизации');
+        notification.error({ message: 'Ошибка авторизации' });
+        console.error('Ошибка авторизации');
     }
-  }
-
-  async login(loginData: FormAuthValues) {
-    try {
-      const response = await fetch(`/auth/login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify(loginData)
-      });
-
-      checkResponse(response);
-
-      const data = await response.json();
-      checkLoginResponse(data);
-
-      localStorage.setItem(TOKEN_NAME, data.access_token);
-      return true;
-    } catch (error: Error | Response | unknown) {
-      if (error instanceof Response) {
-        this.handleError(error);
-      }
-      console.error(error);
-    }
-  }
-
-  get() {
-    console.log('isGetMethod');
-  }
 }
+
+export const apiLogin = async (loginData: FormAuthValues) => {
+    try {
+        const response = await fetch(`/auth/login`, {
+            ...FETCH_OPTIONS,
+            method: 'POST',
+            body: JSON.stringify(loginData)
+        });
+
+        checkResponse(response);
+
+        const data = await response.json();
+        checkLoginResponse(data);
+
+        localStorage.setItem(TOKEN_NAME, data.access_token);
+        return true;
+    } catch (error: Error | Response | unknown) {
+        if (error instanceof Response) {
+            handleError(error);
+        }
+        console.error(error);
+    }
+};
+
+interface Query {
+    [key: string]: any;
+}
+
+function getSearchParams(query: Query | undefined) {
+    if (query) {
+        return '?' + new URLSearchParams(query);
+    }
+    return '';
+}
+
+export const apiGet = async (query: Query | undefined) => {
+    try {
+        const response = await fetch(`/auth/login${getSearchParams(query)}`, {
+            ...FETCH_OPTIONS,
+            method: 'GET'
+        });
+
+        checkResponse(response);
+
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        if (error instanceof Response) {
+            handleError(error);
+        }
+        console.error(error);
+    }
+};
--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -4,63 +4,61 @@ import { Button, Form, Input } from 'antd';
 import styled from 'styled-components';
 
 import { FormAuthValues } from '../type/common';
-import { Api } from '../app/api';
-
-const api = new Api();
+import { apiLogin } from '../app/api';
 
 const Wrapper = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-  form {
-    width: 500px;
-  }
+    form {
+        width: 500px;
+    }
 `;
 
 const AuthPage = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const onSubmit = async (values: FormAuthValues) => {
-    const res = await api.login(values);
+    const onSubmit = async (values: FormAuthValues) => {
+        const res = await apiLogin(values);
 
-    if (res) {
-      navigate('/', { replace: true });
-    }
-  };
+        if (res) {
+            navigate('/', { replace: true });
+        }
+    };
 
-  return (
-    <Wrapper>
-      <Form
-        onFinish={onSubmit}
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-      >
-        <Form.Item
-          label={'Имя пользователя'}
-          name={'username'}
-          rules={[{ required: true, message: 'Заполните поле' }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label={'Пароль'}
-          name={'password'}
-          rules={[{ required: true, message: 'Заполните поле' }]}
-        >
-          <Input.Password />
-        </Form.Item>
+    return (
+        <Wrapper>
+            <Form
+                onFinish={onSubmit}
+                labelCol={{ span: 8 }}
+                wrapperCol={{ span: 16 }}
+            >
+                <Form.Item
+                    label={'Имя пользователя'}
+                    name={'username'}
+                    rules={[{ required: true, message: 'Заполните поле' }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label={'Пароль'}
+                    name={'password'}
+                    rules={[{ required: true, message: 'Заполните поле' }]}
+                >
+                    <Input.Password />
+                </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Отправить
-          </Button>
-        </Form.Item>
-      </Form>
-    </Wrapper>
-  );
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type="primary" htmlType="submit">
+                        Отправить
+                    </Button>
+                </Form.Item>
+            </Form>
+        </Wrapper>
+    );
 };
 
 export default AuthPage;
